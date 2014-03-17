dff5d359109cefc95349b8c62b1ab99e8b94f764
Shayan Pooya <shayan@chango.com>
2014-03-16 19:47:35 -0500

    Cleanup the hdfs example.

--- examples/util/wordcount_hdfs.py
+++ examples/util/wordcount_hdfs.py
@@ -1,9 +1,13 @@
 from disco.core import Job, result_iterator
 from disco.worker.task_io import task_output_stream, plain_output_stream
 
+def getHdfsMaster(discoMaster):
+    from disco.util import schemesplit
+    _, rest = schemesplit(discoMaster)
+    return rest.split(':')[0] + ':50070'
+
 class WordCount(Job):
     save = False
-    save_info = "hdfs,devdisco03:50070,shayan,/user/shayan/"
     reduce_output_stream = (task_output_stream, plain_output_stream)
 
     @staticmethod
@@ -17,14 +21,10 @@ class WordCount(Job):
         for word, counts in kvgroup(sorted(iter)):
             yield word, sum(counts)
 
-def getHdfsMaster(discoMaster):
-    from disco.util import schemesplit
-    _, rest = schemesplit(discoMaster)
-    return rest.split(':')[0] + ':50070'
-
 if __name__ == '__main__':
     from wordcount_hdfs import WordCount
     job = WordCount()
+    job.save_info = "hdfs," + getHdfsMaster(job.disco.master) + ",shayan,/user/shayan/"
     hdfsMaster = getHdfsMaster(job.disco.master)
     job = job.run(input=['hdfs://' + hdfsMaster + '://user/shayan/chekhov'])
     for word, count in result_iterator(job.wait(show=True)):
