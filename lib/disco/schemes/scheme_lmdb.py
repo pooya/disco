bb6e947c8c0aa14ba1f4e27ad7c029971d048a29
Shayan Pooya <shayan@chango.com>
2014-04-14 15:05:19 -0400

    1

--- lib/disco/schemes/scheme_lmdb.py
+++ lib/disco/schemes/scheme_lmdb.py
@@ -4,19 +4,23 @@ import mdb
 
 class LmdbOutputStream(object):
     def __init__(self, stream, url):
-        #print "url is: ", url, "stream is: ", stream, "stream.path is: ", stream.path
         import os
         dir = os.path.dirname(os.path.realpath(url))
         self.env = mdb.Env(dir, max_dbs=8)
-        self.txn = self.env.begin_txn()
-        self.db = self.env.open_db(self.txn, "test_write")
+        from collections import defaultdict
+        self.Dict = defaultdict(list)
         # this is for input
         #self.generator = self.db.items(txn)
 
-    def add(self, key, value):
-        self.db.put(self.txn, key, value)
+    def add(self, k, v):
+        self.Dict[k].append(v)
 
     def close(self):
+        self.txn = self.env.begin_txn()
+        self.db = self.env.open_db(self.txn, "test_write")
+        for k, vs in self.Dict.items():
+            for v in vs:
+                self.db.put(self.txn, k, v)
         self.txn.commit()
         self.db.close()
         self.env.close()
@@ -27,7 +31,9 @@ def lmdb_output(stream, partition, url, params):
 lmdb_stream = (task_output_stream, lmdb_output)
 
 if __name__ == '__main__':
-    File = "/Users/shayan/workspace/disco/data/localhost/eb/WordCount@575:790f4:35ba9/0d/reduce:0--2-5a-66272"
+    import sys
+
+    File = sys.argv[1]
     env = mdb.Env(File, max_dbs=8)
     txn = env.begin_txn()
     db = env.open_db(txn, "test_write")
